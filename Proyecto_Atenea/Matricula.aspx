d935354adffca0b40075a0865aab9569c65d436b
Alejandro Berrio <alejo86-12@hotmail.com>
2016-05-18 22:10:55 -0500

    carga inicial

---
+++ Proyecto_Atenea/Matricula.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Matricula.aspx.cs" Inherits="Proyecto_Atenea.Matricula" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Proyecto_Atenea/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Atenea.Startup))]
+namespace Proyecto_Atenea
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
